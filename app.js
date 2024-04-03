354f72f979daa7052dba3e5287eb920f8da2d79d
sanjay <94178708+Nanjunda3@users.noreply.github.com>
2024-04-03 17:00:25 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+const path = require( 'path' );
+const nodemailer = require('nodemailer');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'rohitstories1@gmail.com', 
+    pass: 'olih ylku eozt vpda' 
+  }
+});
+
+app.post('/sendMail', (req, res) => {
+  const { name, city, phone, email, requirements } = req.body;
+console.log("node mailer is on");
+  // Create the email message
+  const mailOptions = {
+    from: 'rohitstories1@gmail.com', 
+    to: 'hello@spacemandesigns.com', 
+    subject: 'New Registration',
+    html: `<p>New registration at Spaceman Designs</p>
+           <p>Name: ${name}</p>
+           <p>City: ${city}</p>
+           <p>Phone: ${phone}</p>
+           <p>Email: ${email}</p>
+           <p>Requirement: ${requirements}</p>`
+  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.error(error);
+    }
+    console.log('Email sent:', info.response);
+    // res.send('Email sent successfully!');
+    res.render('home', { emailSent: true });
+    // window.location.reload();
+  });
+});
+
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+
+const port = process.env.PORT || 3000;
+server.listen (port ,()=>
+{
+  console.log(`running on locahost ${port}`);
+});
+
+app.get ('/', (req,res)=>
+{
+  res.render('home')
+})
+
+app.get ('/contact', (req, res)=>
+{
+  res.render('contactus')
+})
+
+app.get ('/solution', (req, res)=>
+{
+  res.render('oursolution')
+})
---
+++ assets/home.js
@@ -0,0 +1,87 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById("lottie-container"),
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "../assets/interior.json",
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById("lottie-container2"),
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "../assets/productmanage.json",
+  });
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById("lottie-container3"),
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "../assets/projectmanage.json",
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById("lottie-container1"),
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "../assets/googlesymbol.json",
+  });
+});
+
+function displayPopupForm() {
+  document.getElementById('popupformmain1').style.display = 'block';
+}
+
+function closePopupForm() {
+  document.getElementById('popupformmain1').style.display = 'none';
+}
+
+function handleClickOutside(event) {
+  const popupForm = document.getElementById('popupformmain1');
+  if (!popupForm.contains(event.target)) {
+    closePopupForm();
+  }
+}
+
+
+document.addEventListener('click', handleClickOutside);
+
+
+setInterval(displayPopupForm, 30000);
+
+function startAnimation() {
+  var images = document.querySelectorAll('.image, .image3, .image5');
+  var images1 = document.querySelectorAll( '.image2, .image4' );
+  images1.forEach(function(image) {
+    image.style.animation = 'slide-down 1.5s linear';
+  });
+  images.forEach(function(image) {
+    image.style.animation = 'slide-up 1.5s linear';
+  });
+  setTimeout(function() {
+    images.forEach(function(image) {
+      image.style.animation = ''; // Clear animation after 3 seconds
+    });
+  }, 3000); // Wait for 3 seconds before restarting animation
+  setTimeout(function() {
+    images1.forEach(function(image) {
+      image.style.animation = ''; // Clear animation after 3 seconds
+    });
+  }, 3000);
+}
+
+// Start animation initially
+startAnimation();
+setInterval(startAnimation, 10000);
+

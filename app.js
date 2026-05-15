1dbe0e4617f3985ccd9c139246d5e52ebfff8359
sanjay <94178708+Nanjunda3@users.noreply.github.com>
2026-05-15 22:55:32 +0530

    updates

--- app.js
+++ app.js
@@ -6,43 +6,6 @@ const path = require( 'path' );
 const nodemailer = require('nodemailer');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'rohitstories1@gmail.com', 
-    pass: 'olih ylku eozt vpda' 
-  }
-});
-
-app.post('/sendMail', (req, res) => {
-  const { name, city, phone, email, requirements } = req.body;
-console.log("node mailer is on");
-  // Create the email message
-  const mailOptions = {
-    from: 'rohitstories1@gmail.com', 
-    to: 'hello@spacemandesigns.com', 
-    subject: 'New Registration',
-    html: `<p>New registration at Spaceman Designs</p>
-           <p>Name: ${name}</p>
-           <p>City: ${city}</p>
-           <p>Phone: ${phone}</p>
-           <p>Email: ${email}</p>
-           <p>Requirement: ${requirements}</p>
-           <p>Spaceman design website</P>`
-  };
-
-  // Send the email
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return console.error(error);
-    }
-    console.log('Email sent:', info.response);
-    // res.send('Email sent successfully!');
-    res.render('home', { emailSent: true });
-    // window.location.reload();
-  });
-});
-
 
 
 app.set('view engine', 'ejs');
@@ -58,25 +21,5 @@ server.listen (port ,()=>
 
 app.get ('/', (req,res)=>
 {
-  res.render('home')
-})
-
-app.get ('/contact', (req, res)=>
-{
-  res.render('contactus')
-})
-
-app.get ('/solution', (req, res)=>
-{
-  res.render('oursolution')
-})
-
-app.get ('/gallery', (req, res)=>
-{
-  res.render('gallery')
-})
-
-app.get ('/about', (req, res)=>
-{
-  res.render('about')
+  res.render('blank')
 })
